816a982cd47071ffd62573d1e620831f5b850787
flowerviaw <flow_kusuma_34pg@student.smktelkom-mlg.sch.id>
2026-01-21 13:42:03 +0700

    penambahan nama dan nilai

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 int nilai_siswa;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.Write("Masukkan nama:  ");
+nama_siswa = Console.ReadLine();
+
+Console.Write("Masukkan Nilai:  ");
+nilai_siswa = int.Parse(Console.ReadLine());
