acbf0be7cb1c21994c85a84110b3d9f4f5e6a4c8
flowerviaw <flow_kusuma_34pg@student.smktelkom-mlg.sch.id>
2026-01-21 13:36:12 +0700

    deklarasi variabel nilai dna nama

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string nama_siswa;
+int nilai_siswa;
+
+Console.WriteLine("Aplikasi Nilai Siswa");
